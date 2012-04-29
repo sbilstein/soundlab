ea987699f26e13cd86c35b57abfb4edcf272010e
Siegfried Bilstein <sbilstein@gmail.com>
2012-04-29 15:04:47 -0700

    added default values for up_votes and down_votes

--- app/models/jam.rb
+++ app/models/jam.rb
@@ -2,4 +2,9 @@ class Jam < ActiveRecord::Base
   attr_accessible :song, :title, :up_votes, :down_votes
   default_scope :order => 'up_votes DESC'
   validates :title, :song, presence: true
+  before_save :default_values
+  def default_values
+    self.up_votes = 0
+    self.down_votes = 0
+  end
 end

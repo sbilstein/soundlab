cd1fbb9acbe2c8314107f2f02f6ffb4cc9ed8de5
Siegfried Bilstein <sbilstein@gmail.com>
2012-05-01 22:14:31 -0700

    static controller toughness

---
+++ test/functional/static_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticControllerTest < ActionController::TestCase
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get faq" do
+    get :faq
+    assert_response :success
+  end
+
+end

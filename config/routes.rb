c0a0e5f0faa47c96836f3eea3c65aa2b40892593
Siegfried Bilstein <sbilstein@gmail.com>
2012-05-01 20:29:17 -0700

    routed back to the jams page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Soundlab::Application.routes.draw do
   resources :jams
-  root :to => 'jams#show'
+  root :to => 'jams#index'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.

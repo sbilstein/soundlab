b2b084fa6d82f3b73e53205378994692373a0c57
Siegfried Bilstein <sbilstein@gmail.com>
2012-05-09 21:28:29 -0700

    adding secret code

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120429204407) do
+ActiveRecord::Schema.define(:version => 20120510042813) do
 
   create_table "jams", :force => true do |t|
     t.string   "title"
